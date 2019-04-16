22dcf25aead5cef724c59c4cc9bd59297462c20d
Luke Macken <lmacken@users.noreply.github.com>
2019-04-16 12:59:03 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="Binance DEX SDK",
+    version="1.0",
+    description="Binance DEX SDK",
+    author="",
+    author_email="",
+    url="",
+    packages=["binancedex"],
+)
