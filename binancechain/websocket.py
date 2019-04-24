f75dcd66e110380069304e17cce25362c749ffc2
Luke Macken <lmacken@users.noreply.github.com>
2019-04-23 18:00:14 -0600

    ws: type hinting and various other tweaks

--- binancechain/websocket.py
+++ binancechain/websocket.py
@@ -4,7 +4,7 @@ https://docs.binance.org/api-reference/dex-api/ws-streams.html#websocket-streams
 import asyncio
 import json
 import sys
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Optional
 
 import aiohttp
 
@@ -15,17 +15,28 @@ TESTNET_URL = "wss://testnet-dex.binance.org/api/ws"
 class BinanceDexSocketManager:
     """The Binance DEX WebSocket Manager."""
 
-    def __init__(self, testnet=False) -> None:
+    def __init__(self, testnet: bool = False) -> None:
         self.url = TESTNET_URL if testnet else MAINNET_URL
-        self.session = aiohttp.ClientSession()
-        self.callbacks: Dict[str, Callable[[dict], None]] = {}
+        self._session = aiohttp.ClientSession()
+        self._callbacks: Dict[str, Callable[[dict], None]] = {}
 
-    def start(self, on_open=None, on_error=None, loop=None):
+    def start(
+        self,
+        on_open: Optional[Callable[[], None]] = None,
+        on_error: Optional[Callable[[dict], None]] = None,
+        loop: asyncio.AbstractEventLoop = None,
+    ) -> None:
+        """The main blocking call to start the WebSocket connection."""
         loop = loop or asyncio.get_event_loop()
         return loop.run_until_complete(self._start(on_open, on_error))
 
-    async def _start(self, on_open, on_error):
-        async with self.session.ws_connect(self.url) as ws:
+    async def _start(
+        self,
+        on_open: Optional[Callable[[], None]],
+        on_error: Optional[Callable[[dict], None]],
+    ):
+        """Processes all websocket messages."""
+        async with self._session.ws_connect(self.url) as ws:
             self.running = True
             self.ws = ws
             if on_open:
@@ -37,6 +48,9 @@ class BinanceDexSocketManager:
                     except Exception as e:
                         print(f"Unable to decode msg: {msg}", file=sys.stderr)
                         continue
+                    if not data:
+                        print(f"Got empty msg: {msg}", file=sys.stderr)
+                        continue
                     if "error" in data:
                         if on_error:
                             on_error(data)
@@ -47,12 +61,12 @@ class BinanceDexSocketManager:
                         print(f"Got msg without stream: {data}", file=sys.stderr)
                         continue
                     stream = data["stream"]
-                    if stream not in self.callbacks:
+                    if stream not in self._callbacks:
                         print(
                             f"Cannot find callback for stream {stream}", file=sys.stderr
                         )
                         continue
-                    self.callbacks[stream](data)
+                    self._callbacks[stream](data)
                 elif msg.type == aiohttp.WSMsgType.ERROR:
                     print(msg, file=sys.stderr)
                     break
@@ -78,10 +92,10 @@ class BinanceDexSocketManager:
             payload["symbols"] = symbols
         if address:
             payload["userAddress"] = address
-        self.callbacks[stream] = callback
+        self._callbacks[stream] = callback
         asyncio.ensure_future(self.send(payload))
 
-    def unsubscribe(self, stream, symbols=None):
+    def unsubscribe(self, stream, symbols=None) -> None:
         if not self.ws:
             print(
                 "Cannot subscribe to stream before websocket is initialized.",
@@ -93,19 +107,48 @@ class BinanceDexSocketManager:
             payload["symbols"] = symbols
         asyncio.ensure_future(self.send(payload))
 
-    def subscribe_trades(self, symbols: List[str]) -> None:
-        """Returns individual trade updates."""
-        self.subscribe("trades", symbols=symbols)
+    def subscribe_user_orders(
+        self, address: str, callback: Callable[[dict], None]
+    ) -> None:
+        """Subscribe to individual order updates."""
+        self.subscribe("orders", address=address, callback=callback)
 
-    def subscribe_market_diff(self, symbols: List[str]) -> None:
-        "Order book price and quantity depth updates used to locally keep an order book."""
-        self.subscribe("marketDiff", symbols=symbols)
+    def subscribe_user_accounts(
+        self, address: str, callback: Callable[[dict], None]
+    ) -> None:
+        """Subscribe to account updates."""
+        self.subscribe("accounts", address=address, callback=callback)
 
-    def subscribe_market_depth(self, symbols: List[str]) -> None:
+    def subscribe_user_transfers(
+        self, address: str, callback: Callable[[dict], None]
+    ) -> None:
+        """
+        Subscribe to transfer updates if `address` is involved (as sender or
+        receiver) in a transfer. Multisend is also covered.
+        """
+        self.subscribe("transfers", address=address, callback=callback)
+
+    def subscribe_trades(
+        self, symbols: List[str], callback: Callable[[dict], None]
+    ) -> None:
+        """Subscribe to individual trade updates."""
+        self.subscribe("trades", symbols=symbols, callback=callback)
+
+    def subscribe_market_diff(
+        self, symbols: List[str], callback: Callable[[dict], None]
+    ) -> None:
+        "Order book price and quantity depth updates used to locally keep an order book." ""
+        self.subscribe("marketDiff", symbols=symbols, callback=callback)
+
+    def subscribe_market_depth(
+        self, symbols: List[str], callback: Callable[[dict], None]
+    ) -> None:
         """Top 20 levels of bids and asks."""
-        self.subscribe("marketDepth", symbols=symbols)
+        self.subscribe("marketDepth", symbols=symbols, callback=callback)
 
-    def subscribe_kline(self, interval: str, symbols: List[str]) -> None:
+    def subscribe_kline(
+        self, interval: str, symbols: List[str], callback: Callable[[dict], None]
+    ) -> None:
         """
         The kline/candlestick stream pushes updates to the current
         klines/candlestick every second.
@@ -114,53 +157,57 @@ class BinanceDexSocketManager:
             m -> minutes; h -> hours; d -> days; w -> weeks; M -> months
             1m 3m 5m 15m 30m 1h 2h 4h 6h 8h 12h 1d 3d 1w 1M
         """
-        self.subscribe(f"kline_{interval}", symbols=symbols)
+        self.subscribe(f"kline_{interval}", symbols=symbols, callback=callback)
 
-    def subscribe_ticker(self, symbols: List[str]) -> None:
+    def subscribe_ticker(
+        self, symbols: List[str], callback: Callable[[dict], None]
+    ) -> None:
         """24hr Ticker statistics for a single symbol are pushed every second."""
-        self.subscribe("ticker", symbols=symbols)
+        self.subscribe("ticker", symbols=symbols, callback=callback)
 
-    def subscribe_all_tickers(self) -> None:
+    def subscribe_all_tickers(self, callback: Callable[[dict], None]) -> None:
         """24hr Ticker statistics for a all symbols are pushed every second."""
-        self.subscribe("allTickers", symbols=["$all"])
+        self.subscribe("allTickers", symbols=["$all"], callback=callback)
 
-    def subscribe_mini_ticker(self, symbols: List[str]) -> None:
+    def subscribe_mini_ticker(
+        self, symbols: List[str], callback: Callable[[dict], None]
+    ) -> None:
         """A ticker for a single symbol is pushed every second."""
-        self.subscribe("miniTicker", symbols=symbols)
+        self.subscribe("miniTicker", symbols=symbols, callback=callback)
 
-    def subscribe_all_mini_tickers(self) -> None:
+    def subscribe_all_mini_tickers(self, callback: Callable[[dict], None]) -> None:
         """Array of 24hr Mini Ticker statistics for a all symbols pushed every second."""
-        self.subscribe("allMiniTickers", symbols=["$all"])
+        self.subscribe("allMiniTickers", symbols=["$all"], callback=callback)
 
-    def subscribe_blockheight(self) -> None:
+    def subscribe_blockheight(self, callback: Callable[[dict], None]) -> None:
         """Streams the latest block height."""
-        self.subscribe("blockheight", symbols=["$all"])
-
-    """
-    TODO: user streams, where it needs a seperate /ws/<addy> connection
-
-    Topic Name: orders | Stream: /ws/userAddress
-        { method: "subscribe", topic: "orders", userAddress: "bnc1hp7cves62dzj8n4z8ckna0d3t6zd7z2zcj6gtq" }
-
-    Topic Name: accounts | Stream: /ws/userAddress
-        { method: "subscribe", topic: "accounts", userAddress: "bnc1hp7cves62dzj8n4z8ckna0d3t6zd7z2zcj6gtq" }
-
-
-    Topic Name: transfers | Stream: /ws/userAddress
-        { method: "subscribe", topic: "transfers", userAddress: "bnb1z220ps26qlwfgz5dew9hdxe8m5malre3qy6zr9" }
-    """
+        self.subscribe("blockheight", symbols=["$all"], callback=callback)
 
 
 if __name__ == "__main__":
     dex = BinanceDexSocketManager(testnet=True)
 
     def on_open():
-        dex.subscribe("allMiniTickers", symbols=["$all"], callback=mini_tickers)
+        address = "tbnb18d6rnpmkxzqv3767xaaev5zzn06p42nya8zu79"
+        dex.subscribe_user_orders(address, user_orders)
+        dex.subscribe_user_accounts(address, user_orders)
+        dex.subscribe_user_transfers(address, user_orders)
+
+        # dex.subscribe("allMiniTickers", symbols=["$all"], callback=mini_tickers)
+
+    def user_orders(msg):
+        print(f"user_orders: {msg}")
+
+    def user_accounts(msg):
+        print(f"user_accounts: {msg}")
+
+    def user_transfers(msg):
+        print(f"user_transfers: {msg}")
 
     def mini_tickers(msg):
-        print("mini_tickers:", msg)
+        print(f"mini_tickers: {msg}")
 
     def on_error(msg):
-        print("! on_error", msg)
+        print(f"Error: {msg}")
 
     dex.start(on_open, on_error)
