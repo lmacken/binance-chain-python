76ce9dca351da2e5e8380331bb217db9b040f0a2
Luke Macken <lmacken@users.noreply.github.com>
2019-04-23 19:00:58 -0600

    ws: add keepalive and close methods

--- binancechain/websocket.py
+++ binancechain/websocket.py
@@ -1,4 +1,27 @@
+# Copyright 2019 Sensei.Chat <https://sensei.chat>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# SPDX-License-Identifier: MIT
 """
+Binance DEX WebSockets
+
 https://docs.binance.org/api-reference/dex-api/ws-streams.html#websocket-streams
 """
 import asyncio
@@ -11,7 +34,7 @@ MAINNET_URL = ""
 TESTNET_URL = "wss://testnet-dex.binance.org/api/ws"
 
 
-class BinanceDexSocketManager:
+class BinanceChainWebSocket:
     """The Binance DEX WebSocket Manager."""
 
     def __init__(self, testnet: bool = False) -> None:
@@ -180,9 +203,17 @@ class BinanceDexSocketManager:
         """Streams the latest block height."""
         self.subscribe("blockheight", symbols=["$all"], callback=callback)
 
+    def keepalive(self) -> None:
+        """Extend the connection time by another 30 minutes"""
+        asyncio.ensure_future(self.send({"method": "keepAlive"}))
+
+    def close(self) -> None:
+        """Close the websocket connection"""
+        asyncio.ensure_future(self.send({"method": "close"}))
+
 
 if __name__ == "__main__":
-    dex = BinanceDexSocketManager(testnet=True)
+    dex = BinanceChainWebSocket(testnet=True)
 
     def on_open():
         address = "tbnb18d6rnpmkxzqv3767xaaev5zzn06p42nya8zu79"
