4d3b9990df163906abfd5953c90ffc027f9cadc1
Luke Macken <lmacken@users.noreply.github.com>
2019-04-27 01:38:16 -0600

    setup.py: v0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name="binancechain",
-    version="1.0",
+    version="0.1",
     description="Binance Chain SDK",
     author="Luke Macken & Kim Bui",
     author_email="",
